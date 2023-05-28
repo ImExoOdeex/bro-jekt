63ad08530f1ff80b7e1f47df2377ef3c7dcbea8b
ImExoOdeex <odeex722@gmail.com>
2023-05-28 02:14:02 +0200

    netlify2

--- remix.config.js
+++ remix.config.js
@@ -5,6 +5,11 @@ module.exports = {
 	// assetsBuildDirectory: "public/build",
 	// serverBuildPath: "build/index.js",
 	// publicPath: "/build/",
+	server:
+		process.env.NETLIFY || process.env.NETLIFY_LOCAL
+			? "./server.ts"
+			: undefined,
+	serverBuildPath: ".netlify/functions-internal/server.js",
 	serverModuleFormat: "cjs",
 	future: {
 		v2_errorBoundary: true,
--- server.ts
+++ server.ts
@@ -1,63 +1,10 @@
-import { createRequestHandler } from "@remix-run/express";
-import { broadcastDevReady } from "@remix-run/node";
-import compression from "compression";
-import express, {
-	type NextFunction,
-	type Request,
-	type Response
-} from "express";
-import morgan from "morgan";
-import path from "path";
+import * as build from "@remix-run/dev/server-build";
+import { createRequestHandler } from "@remix-run/netlify";
+import { installGlobals } from "@remix-run/node";
 
-const BUILD_DIR = path.join(process.cwd(), "build");
+installGlobals();
 
-const app = express();
-app.use(compression());
-
-app.disable("x-powered-by");
-app.use(
-	"/build",
-	express.static("public/build", { immutable: true, maxAge: "1y" })
-);
-
-app.use(express.static("public", { maxAge: "1y" }));
-
-app.use(
-	morgan(":req[x-forwarded-for] :method :url :status | :response-time ms")
-);
-
-app.all(
-	"*",
-	process.env.NODE_ENV === "development"
-		? (req: Request, res: Response, next: NextFunction) => {
-				purgeRequireCache();
-
-				return createRequestHandler({
-					build: require(BUILD_DIR),
-					mode: process.env.NODE_ENV
-				})(req, res, next);
-		  }
-		: createRequestHandler({
-				build: require(BUILD_DIR),
-				mode: process.env.NODE_ENV
-		  })
-);
-const port =
-	process.env.PORT || process.env.NODE_ENV === "development" ? 3000 : 3000; // any port you want for production
-
-app.listen(port, () => {
-	console.log(`Express server listening on port ${port}`);
-
-	if (process.env.NODE_ENV === "development") {
-		const build = require(BUILD_DIR);
-		broadcastDevReady(build);
-	}
+export const handler = createRequestHandler({
+	build: build as any,
+	mode: process.env.NODE_ENV
 });
-
-function purgeRequireCache() {
-	for (const key in require.cache) {
-		if (key.startsWith(BUILD_DIR)) {
-			delete require.cache[key];
-		}
-	}
-}
